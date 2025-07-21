8566104191962a512c40f3abf61498c92a00d6b1
tahmaseb73 <Tahmasebimoein140@gmail.com>
2025-07-21 22:43:59 +0330

    Update speedtest2.sh

--- utils/speedtest/speedtest2.sh
+++ utils/speedtest/speedtest2.sh
@@ -1,7 +1,8 @@
-#准备好所需文件
+#!/bin/bash
+# آماده‌سازی فایل‌های موردنیاز
 wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
 gzip -d lite-linux-amd64.gz
 wget -O lite_config.json https://raw.githubusercontent.com/tahmaseb73/Iran_Config_Collector/main/utils/speedtest/lite_config.json
-#运行 LiteSpeedTest
+# اجرای LiteSpeedTest
 chmod +x ./lite-linux-amd64
-sudo nohup ./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/tahmaseb73/Iran_Config_Collector/main/bulk/merge3.txt
+./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/tahmaseb73/Iran_Config_Collector/main/bulk/merge3.txt > speedtest_output.log 2>&1
