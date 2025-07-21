67b8a83f53db24d156c0cd97c9cbfe6dc0a1d7ea
tahmaseb73 <Tahmasebimoein140@gmail.com>
2025-07-21 23:30:01 +0330

    Update speedtest2.sh

--- utils/speedtest/speedtest2.sh
+++ utils/speedtest/speedtest2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # آماده‌سازی فایل‌های موردنیاز
-wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
-gzip -d lite-linux-amd64.gz
-wget -O lite_config.json https://raw.githubusercontent.com/tahmaseb73/Iran_Config_Collector/main/utils/speedtest/lite_config.json
+wget -O utils/speedtest/lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
+gzip -d utils/speedtest/lite-linux-amd64.gz
+wget -O utils/speedtest/lite_config.json https://raw.githubusercontent.com/tahmaseb73/Iran_Config_Collector/main/utils/speedtest/lite_config.json
 # اجرای LiteSpeedTest
-chmod +x ./lite-linux-amd64
-./lite-linux-amd64 --config ./lite_config.json --test https://raw.githubusercontent.com/tahmaseb73/Iran_Config_Collector/main/bulk/merge3.txt > speedtest_output.log 2>&1
+chmod +x utils/speedtest/lite-linux-amd64
+utils/speedtest/lite-linux-amd64 --config utils/speedtest/lite_config.json --test https://raw.githubusercontent.com/tahmaseb73/Iran_Config_Collector/main/bulk/merge3.txt --output utils/speedtest/out.json > utils/speedtest/speedtest_output.log 2>&1
