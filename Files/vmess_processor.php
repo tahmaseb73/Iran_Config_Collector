8178e60a95b2fb616b6ec9cc139dff05fd2ddc03
tahmaseb73 <Tahmasebimoein140@gmail.com>
2025-07-21 23:50:10 +0330

    Update vmess_processor.php

--- Files/vmess_processor.php
+++ Files/vmess_processor.php
@@ -45,8 +45,13 @@ function remove_duplicate_vmess($input)
 
 // Fetch VMess configurations from URL
 $vmess_url = "https://raw.githubusercontent.com/Argh94/V2RayAutoConfig/refs/heads/main/configs/Vmess.txt";
-$vmess_data = @file_get_contents($vmess_url);
+$ch = curl_init($vmess_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FAILONERROR, true);
+$vmess_data = curl_exec($ch);
 if ($vmess_data === false) {
-    file_put_contents("logs/php_error.log", "Error: Could not fetch data from $vmess_url");
+    file_put_contents("logs/php_error.log", "cURL Error: " . curl_error($ch));
+    curl_close($ch);
     exit(1);
 }
+curl_close($ch);
